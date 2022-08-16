97ffb2713167dc3b9f8092d04697f55a75b5eb0d
Joey Tsai <joeytsai@JoeyTsai.local>
2022-08-16 10:44:47 -0400

    Update

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -119,6 +119,12 @@ class NewVisitorTest(LiveServerTestCase):
 
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
         # She starts a new list and sees the input is nicely
         # centered there too
         inputbox.send_keys('testing')
